3978385b48691d59b91765d618fc25b6beb3ee89
MISUMI Masaru <deltam@gmail.com>
2018-04-23 15:09:44 +0900

    init

---
+++ lsd.go
@@ -0,0 +1,50 @@
+package lsd_parametrized
+
+type LevenshteinParam struct {
+	Insert  float64
+	Delete  float64
+	Replace float64
+}
+
+func generateCostFunc(p LevenshteinParam) func(rune, rune, float64, float64, float64) float64 {
+	return func(aRune, bRune rune, diagonal, above, left float64) float64 {
+		cost := diagonal
+		if aRune != bRune {
+			cost += p.Replace
+		}
+		if c := left + p.Insert; c < cost {
+			cost = c
+		}
+		if c := above + p.Delete; c < cost {
+			cost = c
+		}
+		return cost
+	}
+}
+
+func GenerateLsdFunc(p LevenshteinParam) func(string, string) float64 {
+	costFunc := generateCostFunc(p)
+	return func(a, b string) float64 {
+		ar, br := []rune(a), []rune(b)
+		costRow := make([]float64, len(ar)+1)
+		for i, _ := range costRow {
+			costRow[i] = float64(i)
+		}
+
+		next := make([]float64, len(costRow))
+		for bc := 1; bc < len(br)+1; bc++ {
+			next[0] = float64(bc)
+			for i := 1; i < len(next); i++ {
+				next[i] = costFunc(ar[i-1], br[bc-1], costRow[i-1], costRow[i], next[i-1])
+			}
+			costRow, next = next, costRow
+		}
+
+		return costRow[len(costRow)-1]
+	}
+}
+
+func Lsd(a, b string) float64 {
+	f := GenerateLsdFunc(LevenshteinParam{1, 1, 1})
+	return f(a, b)
+}
---
+++ lsd_test.go
@@ -0,0 +1,40 @@
+package lsd_parametrized
+
+import "testing"
+
+func TestGenerateLsdFunc(t *testing.T) {
+	testdata := []struct {
+		A     string
+		B     string
+		Cost  float64
+		Param LevenshteinParam
+	}{
+		{"book", "back", 2, LevenshteinParam{Insert: 1, Delete: 1, Replace: 1}},
+		{"book", "back", 0.2, LevenshteinParam{Insert: 1, Delete: 1, Replace: 0.1}},
+		{"こんにちは", "こんばんは", 2, LevenshteinParam{Insert: 1, Delete: 1, Replace: 1}},
+	}
+
+	for _, d := range testdata {
+		lsd := GenerateLsdFunc(d.Param)
+		if c := lsd(d.A, d.B); c != d.Cost {
+			t.Errorf("lsd(\"%s\", \"%s\") = %f, want %f", d.A, d.B, c, d.Cost)
+		}
+	}
+}
+
+func TestLsd(t *testing.T) {
+	testdata := []struct {
+		A    string
+		B    string
+		Cost float64
+	}{
+		{"book", "back", 2},
+		{"こんにちは", "こんばんは", 2},
+	}
+
+	for _, d := range testdata {
+		if c := Lsd(d.A, d.B); c != d.Cost {
+			t.Errorf("lsd(\"%s\", \"%s\") = %f, want 2", d.A, d.B, d.Cost)
+		}
+	}
+}
