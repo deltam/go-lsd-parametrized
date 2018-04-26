fa8552350a4aa0c82c1a88d4d31f00d21db2771b
MISUMI Masaru <deltam@gmail.com>
2018-04-26 12:03:02 +0900

    Fixed test code

--- lsd_test.go
+++ lsd_test.go
@@ -1,6 +1,14 @@
 package lsd_parametrized
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
+
+func equals(a, b float64) bool {
+	epsilon := 0.000000000000001
+	return math.Abs(a-b) < epsilon
+}
 
 func TestGenerateLsdFunc(t *testing.T) {
 	testdata := []struct {
@@ -13,12 +21,17 @@ func TestGenerateLsdFunc(t *testing.T) {
 		{LevenshteinParam{Insert: 1, Delete: 1, Replace: 0.1}, "book", "back", 0.2},
 		{LevenshteinParam{Insert: 0.1, Delete: 1, Replace: 1}, "book", "back", 2},
 		{LevenshteinParam{Insert: 1, Delete: 0.1, Replace: 1}, "book", "back", 2},
+		{LevenshteinParam{Insert: 0.01, Delete: 1, Replace: 0.1}, "book", "backs", 0.21},
 		{LevenshteinParam{Insert: 1, Delete: 1, Replace: 1}, "こんにちは", "こんばんは", 2},
+		{LevenshteinParam{Insert: 1, Delete: 1, Replace: 0.1}, "こんにちは", "こんばんは", 0.2},
+		{LevenshteinParam{Insert: 1, Delete: 1, Replace: 1}, "book", "board", 3},
+		{LevenshteinParam{Insert: 0.1, Delete: 1, Replace: 1}, "book", "board", 2.1},
+		{LevenshteinParam{Insert: 1, Delete: 1, Replace: 0.1}, "book", "board", 1.2},
 	}
 
 	for _, d := range testdata {
 		lsd := GenerateLsdFunc(d.Param)
-		if c := lsd(d.A, d.B); c != d.Cost {
+		if c := lsd(d.A, d.B); !equals(c, d.Cost) {
 			t.Errorf("lsd(\"%s\", \"%s\") = %f, want %f", d.A, d.B, c, d.Cost)
 		}
 	}
@@ -32,10 +45,11 @@ func TestLsd(t *testing.T) {
 	}{
 		{"book", "back", 2},
 		{"こんにちは", "こんばんは", 2},
+		{"kitten", "sitting", 3},
 	}
 
 	for _, d := range testdata {
-		if c := Lsd(d.A, d.B); c != d.Cost {
+		if c := Lsd(d.A, d.B); !equals(c, d.Cost) {
 			t.Errorf("lsd(\"%s\", \"%s\") = %f, want 2", d.A, d.B, d.Cost)
 		}
 	}
