179244cf83a26331c5633e76d4f9ee610090e795
MISUMI Masaru <deltam@users.noreply.github.com>
2018-09-12 00:42:00 +0900

    Refactored

--- ops.go
+++ ops.go
@@ -1,22 +1,21 @@
 package lsdp
 
 // Nearest returns the nearest string in the specified distance measurer
-func Nearest(dm DistanceMeasurer, raw string, subjects []string) (nearest string, distance float64) {
+func Nearest(dm DistanceMeasurer, orig string, strs []string) (nearest string, distance float64) {
 	type result struct {
 		str  string
 		dist float64
 	}
 
 	ch := make(chan result)
-	for _, sub := range subjects {
+	for _, s := range strs {
 		go func(s string) {
-			d := dm.Distance(raw, s)
-			ch <- result{s, d}
-		}(sub)
+			ch <- result{s, dm.Distance(orig, s)}
+		}(s)
 	}
 
 	initFlag := true
-	for i := 0; i < len(subjects); i++ {
+	for range strs {
 		r := <-ch
 		if initFlag || r.dist < distance {
 			distance = r.dist
@@ -30,7 +29,7 @@ func Nearest(dm DistanceMeasurer, raw string, subjects []string) (nearest string
 // DistanceAll returns slice of distance orig to each strs
 func DistanceAll(dm DistanceMeasurer, orig string, strs []string) []float64 {
 	dists := make([]float64, len(strs))
-	done := make(chan struct{}, len(strs))
+	done := make(chan struct{})
 	for i, s := range strs {
 		go func(i int, s string) {
 			dists[i] = dm.Distance(orig, s)
